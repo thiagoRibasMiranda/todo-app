dfce2105bd9c4a209accf3fd27c7ea960e3bb5b5
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-11-13 18:32:14 -0300

    feat: create the component MenuFilter

---
+++ src/components/MenuFilter/MenuFilter.jsx
@@ -0,0 +1,11 @@
+function MenuFilter() {
+  return (
+    <div>
+      <button type="button">All</button>
+      <button type="button">Active</button>
+      <button type="button">Completed</button>
+    </div>
+  );
+}
+
+export default MenuFilter;

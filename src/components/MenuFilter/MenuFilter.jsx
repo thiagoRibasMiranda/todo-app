58299869166c9479c9b3f4ff5c6825ee60e522c0
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-11-19 09:36:23 -0300

    feat: create function handleSubmit

--- src/components/MenuFilter/MenuFilter.jsx
+++ src/components/MenuFilter/MenuFilter.jsx
@@ -1,9 +1,20 @@
-function MenuFilter() {
+/* eslint-disable react/prop-types */
+function MenuFilter({ changeFilter }) {
+  const handleSubmit = (event) => {
+    changeFilter(event.target.innerText);
+  };
+
   return (
     <div>
-      <button type="button">All</button>
-      <button type="button">Active</button>
-      <button type="button">Completed</button>
+      <button type="button" onClick={handleSubmit}>
+        All
+      </button>
+      <button type="button" onClick={handleSubmit}>
+        Active
+      </button>
+      <button type="button" onClick={handleSubmit}>
+        Completed
+      </button>
     </div>
   );
 }

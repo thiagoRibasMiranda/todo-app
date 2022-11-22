4aad76f15b55f5eb2882cba116c73a479139e86c
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-11-22 12:02:22 -0300

    style: add  icon to the component InputTask

--- src/components/InputTask/InputTask.jsx
+++ src/components/InputTask/InputTask.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
+import { ReactComponent as PlusLogo } from "./icons/plus.svg";
 
 function InputTask({ addTask }) {
   const [value, setValue] = useState("");
@@ -15,15 +16,17 @@ function InputTask({ addTask }) {
 
   return (
     <div className="m-0 m-auto p-3 m-4 m-auto bg-white rounded-md">
-      <input
-        type="text"
-        placeholder="Create a new todo..."
-        value={value}
-        onChange={handleChange}
-      />
-      <button type="button" onClick={handleSubmit}>
-        Add
-      </button>
+      <div className="flex justify-between">
+        <input
+          type="text"
+          placeholder="Create a new todo..."
+          value={value}
+          onChange={handleChange}
+        />
+        <button type="button" onClick={handleSubmit}>
+          <PlusLogo />
+        </button>
+      </div>
     </div>
   );
 }

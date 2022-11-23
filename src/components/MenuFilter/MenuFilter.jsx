3453a9a7cc107294985d42029b14de2905fcea81
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-11-23 12:57:22 -0300

    style: add  more styling to the component MenuF...

--- src/components/MenuFilter/MenuFilter.jsx
+++ src/components/MenuFilter/MenuFilter.jsx
@@ -5,16 +5,18 @@ function MenuFilter({ changeFilter }) {
   };
 
   return (
-    <div>
-      <button type="button" onClick={handleSubmit}>
-        All
-      </button>
-      <button type="button" onClick={handleSubmit}>
-        Active
-      </button>
-      <button type="button" onClick={handleSubmit}>
-        Completed
-      </button>
+    <div className="m-0 m-auto p-0 m-4 m-auto bg-white rounded-md shadow-lg">
+      <div className="flex flex-row justify-evenly  p-3">
+        <button type="button" onClick={handleSubmit}>
+          All
+        </button>
+        <button type="button" onClick={handleSubmit}>
+          Active
+        </button>
+        <button type="button" onClick={handleSubmit}>
+          Completed
+        </button>
+      </div>
     </div>
   );
 }
